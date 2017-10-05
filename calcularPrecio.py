de97506df3907c515ad0af7ed49cf85f7e82f879
Capf96 <32279631+Capf96@users.noreply.github.com>
2017-10-05 13:42:05 -0400

    Arreglando la funcion y agregando nuevos test

--- calcularPrecio.py
+++ calcularPrecio.py
@@ -6,14 +6,20 @@ Created on 2 oct. 2017
 '''
 from datetime import date, time
 from tarifa import Tarifa
+from _datetime import timedelta
 
 '''
 Funcion que calcula el precio de un servicio de acuerdo a las horas de trabajo.
+Si la tarifa es negativa, saltara un error y si el tiempoDeServicio es menor a 15
+minutos o mayor a las horas equivalentes a siete dias de trabajo la funcion retornara
+el valor None.
 
 @param tarifa es un Obtejo Tarifa que representa el precio segun los dias en que se 
                 trabajo (Dias de Semana o Fin de Semana).
 @param tiempoDeServicio es una lista de listas que representa el dia y hora de inicio 
                 y fin del servicio.
+                
+@result precio que tendra el servicio prestado.
 '''
 def calcularPrecio(tarifa, tiempoDeServicio):
     # Si los dias son iguales.
@@ -27,6 +33,9 @@ def calcularPrecio(tarifa, tiempoDeServicio):
         # Se calculan las horas trabajadas.
         trabajado = tiempoDeServicio[1][1].hour - tiempoDeServicio[0][1].hour 
         
+        if trabajado == 0 and (tiempoDeServicio[1][1].minute - tiempoDeServicio[0][1].minute < 15):
+            return None 
+        
         # Se suma como 1 hora de trabajo mas si se paso por al menos un segundo.
         if tiempoDeServicio[1][1].minute > tiempoDeServicio[0][1].minute \
                 or tiempoDeServicio[1][1].second > tiempoDeServicio[0][1].second: 
@@ -38,16 +47,16 @@ def calcularPrecio(tarifa, tiempoDeServicio):
     else:
         # Se cuentan los dias de trabajo durante la semana y los dias de
         # trabajo durante el fin de semana.
-        contador = (int(tiempoDeServicio[0][0].weekday()) + 1) % 7
+        contador = tiempoDeServicio[0][0] + timedelta(1)
         diasSemana = 0
         diasFin = 0
-        while contador != tiempoDeServicio[1][0].weekday():
-            if contador < 5: 
+        while contador != tiempoDeServicio[1][0]:
+            if contador.weekday() < 5: 
                 diasSemana += 1
             else:
                 diasFin += 1
-            contador += 1
-            contador %= 7
+            contador += timedelta(1)
+        
             
         # Elegimos la tarifa correspondiente al primer dia del servicio.
         if tiempoDeServicio[0][0].weekday() < 5:
@@ -65,24 +74,22 @@ def calcularPrecio(tarifa, tiempoDeServicio):
         
         # Sumamos como 1 hora de trabajo mas si se paso por al menos un segundo.
         horasDiaFinal = tiempoDeServicio[1][1].hour 
-        if tiempoDeServicio[0][1] == tiempoDeServicio[1][1] and \
+        
+    
+        if tiempoDeServicio[0][1].hour == tiempoDeServicio[1][1].hour and \
                 (tiempoDeServicio[1][1].minute > tiempoDeServicio[0][1].minute \
                     or tiempoDeServicio[1][1].second > tiempoDeServicio[0][1].second): 
             horasDiaFinal += 1
             
         precioUltimoDia = horasDiaFinal*montoHoraDiaFinal  ##Total a pagar por horas de sercios del ultimo dia.
         
-        pagoSemana = diasSemana*24*tarifa.habiles   # Total a pagar por las horas trabajadas durante la semana.
-        pagoFin = diasFin*24*tarifa.finde           # Total a pagar pir las horas trabajadas durante los fin de semana.
+        precioSemana = diasSemana*24*tarifa.habiles   # Total a pagar por las horas trabajadas durante la semana.
+        precioFin = diasFin*24*tarifa.finde           # Total a pagar pir las horas trabajadas durante los fin de semana.
         
-        return precioPrimerDia + pagoSemana + pagoFin + precioUltimoDia       # Total a pagar
-                
-tarifa = Tarifa(1, 2)
-fechaInicio = date(2017, 8, 8)
-fechaFin = date(2017, 8, 16)
-tiempoInicio = time(8)
-tiempoFin = time(0)
-inicio = [fechaInicio, tiempoInicio]
-fin = [fechaFin, tiempoFin]
-tiempoDeServicio = [inicio, fin]
-print(calcularPrecio(tarifa, tiempoDeServicio))
+        if (diasFin + diasSemana + 1) >= 7 \
+                and (diasSemana*24 + diasFin*24 + horasDiaFinal + (24 - tiempoDeServicio[0][1].hour)) > 168:
+            return None
+        
+        
+        return precioPrimerDia + precioSemana + precioFin + precioUltimoDia       # Total a pagar.
+
